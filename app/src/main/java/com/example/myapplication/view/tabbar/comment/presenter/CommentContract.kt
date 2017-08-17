b7fbe8a4d5b8d64990f94d3f3cb0743eedd2ca99
hyelim0716 <hyelim0716@naver.com>
2017-08-17 13:50:44 +0900

    Add not added files

---
+++ app/src/main/java/com/example/myapplication/view/tabbar/comment/presenter/CommentContract.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.view.tabbar.comment.presenter
+
+/**
+ * Created by Owner on 2017-08-17.
+ */
+interface CommentContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+        val view: View
+
+    }
+}
